03eab0871711f629fa2cc952273d07aaa5d7b9b1
DarshD22 <dujrai.darsh0122@gmail.com>
2025-07-01 16:03:14 +0530

    added cors

--- tsk-webhook/app/__init__.py
+++ tsk-webhook/app/__init__.py
@@ -3,7 +3,7 @@ from app.extensions import mongo
 from app.webhook.routes import webhook
 from dotenv import load_dotenv
 import os
-
+from flask_cors import CORS
 # Load environment variables from .env file
 load_dotenv()
 
@@ -14,14 +14,14 @@ def create_app():
     # Set MongoDB URI from environment
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     
-    
+
     # Ensure Mongo URI is set
     if not app.config["MONGO_URI"]:
         raise ValueError("MONGO_URI not set. Please define it in your environment or .env file.")
 
     # Initialize Mongo extension
     mongo.init_app(app)
-
+    CORS(app)
     # Register all blueprints
     app.register_blueprint(webhook)
     
